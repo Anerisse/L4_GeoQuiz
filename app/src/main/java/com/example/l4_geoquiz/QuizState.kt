06d63ec2ba13939af46ffb28cc6a3b83a95096b4
Anerisse <aleksandraneri165@gmail.com>
2025-11-06 17:31:04 +0700

    QuizRepository & QuizState

---
+++ app/src/main/java/com/example/l4_geoquiz/QuizState.kt
@@ -0,0 +1,8 @@
+package com.example.l4_geoquiz
+
+data class QuizState (
+    val currentQuestionIndex: Int = 0,
+    val userAnswers: List<Boolean?> = emptyList(),
+    val isQuizFinished: Boolean = false,
+    val score: Int = 0
+)
