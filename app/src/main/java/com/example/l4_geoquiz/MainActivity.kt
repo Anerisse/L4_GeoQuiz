f1032265c14689ff4a1310b535e74d77b27c86fb
Anerisse <aleksandraneri165@gmail.com>
2025-11-06 17:55:08 +0700

    UI

--- app/src/main/java/com/example/l4_geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/l4_geoquiz/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.l4_geoquiz.ui.theme.L4_GeoQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             L4_GeoQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    QuizShow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun QuizShow( modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+    ){
+        Text(
+            text = "Текст вопроса",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Button(onClick = { /* TODO */ }) {
+                Text("TRUE")
+            }
+            Button(onClick = { /* TODO */ }) {
+                Text(text = "FALSE")
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Row (modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End){
+            Button(onClick = { /* TODO */ }) {
+                Text("NEXT")
+
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun QuizShowPreview() {
     L4_GeoQuizTheme {
-        Greeting("Android")
+        QuizShow()
     }
 }
